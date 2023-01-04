1218edf7b6d7e4ee84f09ed95f2cdd2deba2dd05
Willy Antunez <willyantunezg@gmail.com>
2023-01-04 16:16:47 -0600

    Fin seccion 17

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,36 @@
+
+import { AddOutlined } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
+
+
+
+export const JournalPage = () => {
+  return (
+    <JournalLayout>
+
+      {/* <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias provident molestias aperiam vel cum sit illo amet ipsam quae delectus voluptas, impedit mollitia. Neque modi laboriosam voluptatum repudiandae eveniet assumenda!
+      </Typography> */}
+
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': {backgroundColor: 'error.main', opacity: 0.9},
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{fontSize: 30}} />
+      </IconButton>
+
+    </JournalLayout>
+  )
+}
